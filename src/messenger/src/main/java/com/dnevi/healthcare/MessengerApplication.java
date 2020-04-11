3cf4676e57d1609f63bef2fed190f77797bae73f
DarioNevistic <nevistic.dario@nsoft.com>
2020-04-11 18:34:21 +0200

    Add messenger module

---
+++ src/messenger/src/main/java/com/dnevi/healthcare/MessengerApplication.java
@@ -0,0 +1,13 @@
+package com.dnevi.healthcare;
+
+/**
+ * Hello world!
+ *
+ */
+public class MessengerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
